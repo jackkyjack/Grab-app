182c40da75906ad5d2e82d7012378e0dc89bd705
Jackkyjack <akeraanucha1@gmail.com>
2024-08-30 01:04:38 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Grab-app',
+  webDir: 'www'
+};
+
+export default config;
